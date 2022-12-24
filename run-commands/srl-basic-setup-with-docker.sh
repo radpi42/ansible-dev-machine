2497e209fc21eac6f2b6a4c76fbbce41b3af189d
srl-v3-ansible <radpivm@gmail.com>
2022-12-24 13:01:40 +0000

    updated run command

--- run-commands/srl-basic-setup-with-docker.sh
+++ run-commands/srl-basic-setup-with-docker.sh
@@ -1,1 +1,1 @@
-ansible-playbook /home/radpivm/ansible/playbooks/srl-basic-setup.ansible.yml --user radpivm --ask-pass --ask-become-pass -i /home/radpivm/ansible/inventory/hosts
+ansible-playbook /home/radpivm/ansible-dev-machine/playbooks/srl-basic-setup.ansible.yml --user radpivm --ask-pass --ask-become-pass -i /home/radpivm/ansible-dev-machine/inventory/hosts
